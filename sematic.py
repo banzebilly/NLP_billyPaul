576c492d3fcb22e5a40118b1da8341fb516039f5
banzebilly <121959520+banzebilly@users.noreply.github.com>
2023-11-14 10:52:53 -0800

    Update sematic.py

--- sematic.py
+++ sematic.py
@@ -1,26 +1,49 @@
 import spacy
 
 # Load the 'en_core_web_md' model
-nlp = spacy.load('en_core_web_md')
+nlp_md = spacy.load('en_core_web_md')
+
+# Load the 'en_core_web_sm' model
+nlp_sm = spacy.load('en_core_web_sm')
 
 # Create spaCy Doc objects for the words 'cat,' 'monkey,' and 'banana'
-cat = nlp('cat')
-monkey = nlp('monkey')
-banana = nlp('banana')
+cat = nlp_md('cat')
+monkey = nlp_md('monkey')
+banana = nlp_md('banana')
 
-# Calculate and print the similarity scores between these words
-print("Similarity between 'cat' and 'monkey':", cat.similarity(monkey))
-print("Similarity between 'banana' and 'monkey':", banana.similarity(monkey))
-print("Similarity between 'banana' and 'cat':", banana.similarity(cat))
+# Calculate and print the similarity scores between these words using the medium model
+print("Similarity between 'cat' and 'monkey' (md):", cat.similarity(monkey))
+print("Similarity between 'banana' and 'monkey' (md):", banana.similarity(monkey))
+print("Similarity between 'banana' and 'cat' (md):", banana.similarity(cat))
 
-# Tokenize the input text 'cat apple monkey banana'
-input_text = 'cat apple monkey banana'
-input_doc = nlp(input_text)
+# Calculate and print the similarity scores between these words using the small model
+cat_sm = nlp_sm('cat')
+monkey_sm = nlp_sm('monkey')
+banana_sm = nlp_sm('banana')
+print("\nSimilarity between 'cat' and 'monkey' (sm):", cat_sm.similarity(monkey_sm))
+print("Similarity between 'banana' and 'monkey' (sm):", banana_sm.similarity(monkey_sm))
+print("Similarity between 'banana' and 'cat' (sm):", banana_sm.similarity(cat_sm))
 
-# Iterate through the tokens and calculate the similarity between all token pairs
-print("Similarity scores between all token pairs in the input text:")
-for token1 in input_doc:
-    for token2 in input_doc:
+# Tokenize the input text 'cat apple monkey banana' using the medium model
+input_text_md = 'cat apple monkey banana'
+input_doc_md = nlp_md(input_text_md)
+
+# Tokenize the input text 'cat apple monkey banana' using the small model
+input_text_sm = 'cat apple monkey banana'
+input_doc_sm = nlp_sm(input_text_sm)
+
+# Iterate through the tokens and calculate the similarity between all token pairs using the medium model
+print("\nSimilarity scores between all token pairs in the input text (md):")
+for token1 in input_doc_md:
+    for token2 in input_doc_md:
+        if token1 != token2:
+            similarity_score = token1.similarity(token2)
+            print(f"{token1.text} - {token2.text}: {similarity_score}")
+
+# Iterate through the tokens and calculate the similarity between all token pairs using the small model
+print("\nSimilarity scores between all token pairs in the input text (sm):")
+for token1 in input_doc_sm:
+    for token2 in input_doc_sm:
         if token1 != token2:
             similarity_score = token1.similarity(token2)
             print(f"{token1.text} - {token2.text}: {similarity_score}")
@@ -35,12 +58,29 @@ sentences_to_compare = [
     "Banana and monkey are in the tree."
 ]
 
-# Tokenize the model sentence
-model_sentence_doc = nlp(model_sentence)
+# Tokenize the model sentence using the medium model
+model_sentence_doc_md = nlp_md(model_sentence)
 
-# Iterate through the list of sentences to compare and calculate similarity
-print("Similarity between the model sentence and each sentence in the list:")
+# Tokenize the model sentence using the small model
+model_sentence_doc_sm = nlp_sm(model_sentence)
+
+# Iterate through the list of sentences to compare and calculate similarity using the medium model
+print("\nSimilarity between the model sentence and each sentence in the list (md):")
 for sentence in sentences_to_compare:
-    sentence_doc = nlp(sentence)
-    similarity_score = model_sentence_doc.similarity(sentence_doc)
+    sentence_doc = nlp_md(sentence)
+    similarity_score = model_sentence_doc_md.similarity(sentence_doc)
     print(f"{model_sentence} - {sentence}: {similarity_score}")
+
+# Iterate through the list of sentences to compare and calculate similarity using the small model
+print("\nSimilarity between the model sentence and each sentence in the list (sm):")
+for sentence in sentences_to_compare:
+    sentence_doc = nlp_sm(sentence)
+    similarity_score = model_sentence_doc_sm.similarity(sentence_doc)
+    print(f"{model_sentence} - {sentence}: {similarity_score}")
+
+# Observations:
+# - Similarity scores may vary between the medium and small models.
+# - Larger models generally capture more nuanced relationships between words.
+# - In some cases, the small model may provide reasonable similarity scores.
+# - The choice of model depends on the specific use case and available resources.
+
